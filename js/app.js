90ccc0bb09494a3785582388110d25f07f918ac7
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-17 09:37:45 -0600

    made modal

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const openModal = document.querySelector('body > button');
+const modalBody = document.querySelector('.modal');
+const modalBtn = document.querySelector('.modalBox button');
+
+openModal.addEventListener('click', () => {
+	modalBody.classList.toggle('displayNone');
+});
+
+modalBody.addEventListener('click', (e) => {
+	if (e.target.className === 'modal') {
+		modalBody.classList.toggle('displayNone');
+	}
+});
+modalBtn.addEventListener('click', () => {
+	modalBody.classList.toggle('displayNone');
+});
